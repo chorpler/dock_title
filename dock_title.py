24aed42ebc4023c410b4c589e8df3bd86d62e007
David Sargeant <chorpler@gmail.com>
2023-12-25 16:04:38 -0700

    Update TextStyle class to add more colors and styles

--- dock_title.py
+++ dock_title.py
@@ -16,6 +16,19 @@ from typing import Any, List, Dict, TypedDict, Optional, cast
 plist_file = "~/Library/Preferences/com.apple.dock.plist"
 plist_file_backup = "~/Library/Preferences/com.apple.dock.plist.backup"
 
+
+class TextStyle:  # Define text styles
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    BLUE = '\033[34m'
+    MAGENTA = '\033[35m'
+    CYAN = '\033[36m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    reset = '\033[0m'
+    NC = reset
+
 class textStyle: # Define text styles
    BOLD = '\033[1m'
    underline = '\033[4m'
